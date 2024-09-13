a143507b307666864f6d39cd230322e181cf63f4
vrfdivino <vrfdivino@gmail.com>
2024-09-13 21:18:23 +0800

    feaet: build

--- services/firebase.ts
+++ services/firebase.ts
@@ -5,12 +5,12 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDcit06jXzX8ne2SkckXxnbTKW0rzX4kbA",
-  authDomain: "volito-test.firebaseapp.com",
-  projectId: "volito-test",
-  storageBucket: "volito-test.appspot.com",
-  messagingSenderId: "959636977041",
-  appId: "1:959636977041:web:115378007231d2535c9630",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
