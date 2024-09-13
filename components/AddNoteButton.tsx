6e313fcb4244f97f79b23159189860405e5f0576
vrfdivino <vrfdivino@gmail.com>
2024-09-13 14:55:04 +0800

    feat: add note button

--- components/AddNoteButton.tsx
+++ components/AddNoteButton.tsx
@@ -1,5 +1,9 @@
 import { useRouter } from "expo-router";
-import { Pressable, Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet, TouchableOpacity } from "react-native";
+
+import { COLORS } from "@/constants/theme";
+import Typography from "@/components/Typography";
 
 const AddNoteButton = () => {
   // hooks
@@ -12,18 +16,42 @@ const AddNoteButton = () => {
 
   // render
   return (
-    <Pressable
-      style={{
-        position: "absolute",
-        bottom: 20,
-        right: 20,
-        zIndex: 999,
-      }}
-      onPress={onPress}
-    >
-      <Text>+ Add note</Text>
-    </Pressable>
+    <TouchableOpacity style={styles.pressable} onPress={onPress}>
+      <Ionicons name="add" style={styles.icon} />
+      <Typography
+        variant="bodySmall"
+        text="Add note"
+        customStyle={styles.text}
+      />
+    </TouchableOpacity>
   );
 };
 
 export default AddNoteButton;
+
+const styles = StyleSheet.create({
+  pressable: {
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: COLORS.primary,
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 5,
+    shadowColor: "#171717",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+  },
+  text: {
+    color: COLORS.white,
+    fontWeight: "bold",
+  },
+  icon: {
+    fontSize: 14,
+    color: COLORS.white,
+  },
+});
